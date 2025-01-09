181c9a92cb68270a43e490467a458e5e39470b30
Santosh Bhaskal <148711667+santosh-bhaskal-02@users.noreply.github.com>
2025-01-09 12:19:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server:{
     proxy:{
-      "/api": 'http://localhost:2000'
+      "/api": 'https://ganesh-ecom-back-end.onrender.com'
     }
   },
   plugins: [react()]
